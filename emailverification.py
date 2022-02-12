c0d132d5e2857f455246d5f76971bd693c093db9
Michał Nawarecki <michalnawarecki@wp.pl>
2022-02-12 23:08:43 +0100

    added email verification script

--- emailverification.py
+++ emailverification.py
@@ -1,1 +1,9 @@
 #write program that checks if email is correct
+import re
+from pathlib import Path
+textFile = Path('emails.txt').read_text()
+textFile = textFile.replace('\n','')
+matches = re.findall('([a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+)', textFile)
+print("Here's a list of good emails!")
+for x in matches:
+    print(x)
