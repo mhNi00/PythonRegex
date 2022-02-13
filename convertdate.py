01695ed4c05f0a2b3a43982dbd8a090656c56013
Michał Nawarecki <michalnawarecki@wp.pl>
2022-02-13 13:06:41 +0100

    convertdate script and checkifdecimals scripit

--- convertdate.py
+++ convertdate.py
@@ -1,1 +1,8 @@
 #Write a program that will convert mm-dd-yyyy to dd-mm-yyyy
+import re
+
+date = "Today is 11-29-2022"
+
+print("This regex formula replaces mm-dd-yyyy format with dd-mm-yyy format")
+print(re.sub("([0-9]{2})-([0-9]{2})-([0-9]{4})",r'\2-\1-\3', date))
+
--- isdecimal.py
+++ isdecimal.py
@@ -1,1 +1,17 @@
 #Write a program that will check if a number has a precision of 2
+import re
+
+number = (21
+ ,345.1
+ ,9031.93
+ ,10323.231
+ ,1.31231
+ ,1
+ ,0
+ ,-123.43
+ ,-13.214 )
+print("This program returns true if the number's decimal point has a precision of up to 2.")
+for i in range(len(number)):
+    stringForTesting = str(number[i])
+    print(stringForTesting, ":", bool(re.search('(^-?[0-9]*$)|(^-?[0-9]*\.[0-9]{1,2}$)', stringForTesting)))
+
